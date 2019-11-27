9360ea2e2ea128e968ed2cccffb7f7c1f061741a
WsmLy <1536264016@qq.comssh-keygen -t rsa -C 1536264016@qq.com>
2019-11-27 21:23:16 +0800

    定位

---
+++ app/src/main/java/teotw/com/mywidgets/getLocationWithAmap/GetLocationActivity.java
@@ -0,0 +1,15 @@
+package teotw.com.mywidgets.getLocationWithAmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import teotw.com.mywidgets.R;
+
+public class GetLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location);
+    }
+}
