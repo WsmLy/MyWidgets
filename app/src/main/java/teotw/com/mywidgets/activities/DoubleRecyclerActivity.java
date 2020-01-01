d19321bf633316451561f2852d8aae9eaf211d71
WsmLy <1536264016@qq.comssh-keygen -t rsa -C 1536264016@qq.com>
2020-01-01 22:57:00 +0800

    创建doublerecyclerview

---
+++ app/src/main/java/teotw/com/mywidgets/activities/DoubleRecyclerActivity.java
@@ -0,0 +1,15 @@
+package teotw.com.mywidgets.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import teotw.com.mywidgets.R;
+
+public class DoubleRecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_double_recycler);
+    }
+}
